23ad151391de7acb5c7840420ab3ecf18082b5d7
leohawke <lia77593@163.com>
2017-06-22 12:07:51 +0800

    expression SFNAE?

---
+++ equality_operator.cpp
@@ -0,0 +1,99 @@
+#include "type_traits.hpp"
+
+
+#define limpl(...) __VA_ARGS__
+
+#	define lconstfn constexpr
+#	define LB_PURE
+
+#	define lnoexcept noexcept
+#define lnoexcept_spec(...) lnoexcept(noexcept(__VA_ARGS__))
+
+#	define lnothrow lnoexcept
+
+
+#	define LB_STATELESS
+
+#	define lthrow throw
+
+#	define lnothrowv
+
+namespace sad {
+	using std::enable_if_t;
+
+	template<typename _type, typename _type2>
+	using equality_operator_t
+		= decltype(std::declval<_type>() == std::declval<_type2>());
+
+	template<typename _type, typename _type2 = _type>
+	struct has_equality_operator : is_detected_convertible<bool,
+		equality_operator_t, _type, _type2>
+	{};
+
+	namespace examiners
+	{
+
+		/*!
+		\brief 基本等于操作检测。
+		*/
+		struct equal
+		{
+			template<typename _type1, typename _type2>
+			static lconstfn LB_PURE auto
+				are_equal(_type1&& x, _type2&& y)
+				lnoexcept_spec(bool(x == y)) -> decltype(bool(x == y))
+			{
+				return bool(x == y);
+			}
+		};
+
+
+		/*!
+		\brief 基本等于操作检测：总是相等。
+		*/
+		struct always_equal
+		{
+			template<typename _type, typename _type2, limpl(typename
+				= enable_if_t<!has_equality_operator<_type&&, _type2&&>::value>)>
+				static lconstfn LB_STATELESS bool
+				are_equal(_type&&, _type2&&) lnothrow
+			{
+				return true;
+			}
+		};
+
+
+		/*!
+		\brief 等于操作检测。
+		*/
+		struct equal_examiner : public equal, public always_equal
+		{
+			using equal::are_equal;
+			using always_equal::are_equal;
+		};
+
+	} // namespace examiners;
+
+	template<typename _type1, typename _type2>
+	struct HeldEqual : private examiners::equal_examiner
+	{
+		using examiners::equal_examiner::are_equal;
+	};
+
+	template<typename _type1, typename _type2>
+	lconstfn bool
+		AreEqualHeld(const _type1& x, const _type2& y)
+	{
+		return HeldEqual<_type1, _type2>::are_equal(x, y);
+	}
+
+}
+
+#include <iostream>
+#include <fstream>
+
+int main() {
+	//error C2398->c3770->c2672->c2783
+	std::cout<<sad::AreEqualHeld(std::ifstream(), std::ifstream())<<std::endl;
+	std::cout << sad::AreEqualHeld(1, 2) << std::endl;
+}
---
+++ type_traits.hpp
@@ -0,0 +1,82 @@
+#ifndef T_H
+#define T_H 1
+
+#include <type_traits>
+
+namespace sad {
+	using std::is_convertible;
+	using std::true_type;
+	using std::false_type;
+	using std::void_t;
+	using std::is_same;
+
+	template<class _type>
+	using _t = typename _type::type;
+
+	/*!
+	\brief 不接受任意参数类型构造的类型。
+	\see WG21 N4502 6.6 。
+	\since build 1.4
+	*/
+	struct nonesuch
+	{
+		nonesuch() = delete;
+		nonesuch(const nonesuch&) = delete;
+		~nonesuch() = delete;
+
+		void
+			operator=(const nonesuch&) = delete;
+	};
+
+	/*!
+	\ingroup metafunctions
+	\since build 1.4
+	\see WG21 N4502 。
+	*/
+	//@{
+	namespace details
+	{
+
+		template<typename _tDefault, typename, template<typename...> class, typename...>
+		struct detector
+		{
+			using value_t = false_type;
+			using type = _tDefault;
+		};
+
+		template<typename _tDefault, template<typename...> class _gOp,
+			typename... _tParams>
+			struct detector<_tDefault, void_t<_gOp<_tParams...>>, _gOp, _tParams...>
+		{
+			using value_t = true_type;
+			using type = _gOp<_tParams...>;
+		};
+
+	} // namespace details;
+
+	template<template<typename...> class _gOp, typename... _tParams>
+	using is_detected
+		= typename details::detector<nonesuch, void, _gOp, _tParams...>::value_t;
+
+	template<template<typename...> class _gOp, typename... _tParams>
+	using detected_t = _t<details::detector<nonesuch, void, _gOp, _tParams...>>;
+
+	template<typename _tDefault, template<typename...> class _gOp,
+		typename... _tParams>
+		using detected_or = details::detector<_tDefault, void, _gOp, _tParams...>;
+
+	template<typename _tDefault, template<typename...> class _gOp,
+		typename... _tParams>
+		using detected_or_t = _t<detected_or<_tDefault, _gOp, _tParams...>>;
+
+	template<typename _tExpected, template<typename...> class _gOp,
+		typename... _tParams>
+		using is_detected_exact = is_same<_tExpected, detected_t<_gOp, _tParams...>>;
+
+	template<typename _tTo, template<typename...> class _gOp, typename... _tParams>
+	using is_detected_convertible
+		= is_convertible<detected_t<_gOp, _tParams...>, _tTo>;
+	//@}
+}
+
+#endif
